95f3ff4383bb552274f5571eb197e81787d7c9c8
Alexandre <AlexandreOSouza>
2022-08-19 19:10:36 -0300

    chore: add drawer

--- components/nav/index.jsx
+++ components/nav/index.jsx
@@ -1,11 +1,23 @@
-import { Flex, Text } from "@chakra-ui/react"
+import { Button, Drawer, DrawerBody, DrawerCloseButton, DrawerContent, DrawerHeader, DrawerOverlay, Flex, IconButton, Input, Text } from "@chakra-ui/react"
 import Image from "next/image"
 import CartButton from "../buttons/cart"
 import SearchInput from "../input/search"
+import {useRef, useState} from 'react'
+import { HamburgerIcon } from "@chakra-ui/icons"
 
 const Nav = () => {
+    const btnRef = useRef()
+    const [isOpen, setIsOpen] = useState()
+    const onOpen = () => {
+        setIsOpen(true)
+    }
+
+    const onClose = () => {
+        setIsOpen(false)
+    }
     return (
-        <Flex width={'100%'} justifyContent={'space-between'} alignItems={'center'}>
+        <>
+        <Flex width={'100%'} justifyContent={'space-between'} alignItems={'center'} display={{base: 'none', md: 'flex'}}>
             <Image src={'/static/images/logo_coffe.svg'} width={'150px'} height={'30px'} />
 
             <Flex justifyContent={'space-between'} width={'300px'}>
@@ -24,6 +36,29 @@ const Nav = () => {
                 <CartButton />
             </Flex>
         </Flex>
+        <Flex width={'100%'} justifyContent={'flex-end'}>
+            <IconButton icon={<HamburgerIcon />} ref={btnRef} colorScheme='teal' onClick={onOpen}/>
+        </Flex>
+        <Drawer
+            isOpen={isOpen}
+            placement='right'
+            onClose={onClose}
+            finalFocusRef={btnRef}
+        >
+            <DrawerOverlay />
+            <DrawerContent>
+
+                <DrawerCloseButton />
+                <DrawerHeader borderBottomWidth='1px'>Basic Drawer</DrawerHeader>
+
+                <DrawerBody>
+                    <Input placeholder='Type here...' />
+                </DrawerBody>
+
+                
+            </DrawerContent>
+        </Drawer>
+        </>
     )
 }
 
