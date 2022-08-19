f5e41771c754c46481a116f875f7d289f5c0eac0
Alexandre <AlexandreOSouza>
2022-08-19 17:49:58 -0300

    chore: fix newsletter

--- components/sections/newsletter/index.jsx
+++ components/sections/newsletter/index.jsx
@@ -6,13 +6,14 @@ const NewsletterSection = () => {
     return (
         <Flex width={'100%'} justifyContent={'center'} marginTop={'120px'}>
             <Flex 
-                borderRadius={'24px'}
+                borderRadius={{base: 0, md: '24px'}}
                 backgroundImage={'/static/images/newsletter/bg.png'} 
-                width={'1170px'}   
+                width={{base: '100vw', md: '1170px'}}   
                 height={'292px'}
                 alignItems={'center'}
                 justifyContent={'center'}
                 flexDir={'column'}
+                backgroundPosition={{base: 'top', md: ''}}
             >
                 <TitleText color={'white'} mb={'26px'}>
                     Subscribe to get 50% discount price
